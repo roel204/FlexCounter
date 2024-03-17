b25033dfb7a97628ac807391163a01200abef7e3
roelh <roelhoogendoorn01369@gmail.com>
2024-03-17 21:25:06 +0100

    Added prediction model

--- src/CounterPage.jsx
+++ src/CounterPage.jsx
@@ -1,10 +1,29 @@
-import {useRef} from 'react';
+import {useEffect, useRef} from 'react';
+import {PoseLandmarker, FilesetResolver, DrawingUtils,} from '@mediapipe/tasks-vision'
 
 function CounterPage() {
     const videoElement = useRef(null)
+    const canvasElement = useRef(null)
     const enableWebcamButton = useRef(null)
     let webcamRunning = false;
+    const videoHeight = "720px";
+    const videoWidth = "1280px";
 
+    let runningMode = "VIDEO";
+    let poseLandmarker = undefined;
+
+    const createPoseLandmarker = async () => {
+        const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+        poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
+            baseOptions: {
+                modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+                delegate: "GPU"
+            },
+            runningMode: runningMode,
+            numPoses: 2
+        });
+    };
+    createPoseLandmarker();
 
     // Check if webcam access is supported.
     const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
@@ -18,10 +37,10 @@ function CounterPage() {
     function enableCam() {
         console.log("start the webcam")
 
-        // if (!poseLandmarker) {
-        //     console.log("Wait! poseLandmaker not loaded yet.");
-        //     return;
-        // }
+        if (!poseLandmarker) {
+            console.log("Wait! poseLandmaker not loaded yet.");
+            return;
+        }
 
         if (webcamRunning === true) {
             webcamRunning = false;
@@ -39,10 +58,52 @@ function CounterPage() {
         // Activate the webcam stream.
         navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
             videoElement.current.srcObject = stream;
-            // videoElement.current.addEventListener("loadeddata", predictWebcam);
+            videoElement.current.addEventListener("loadeddata", predictWebcam);
         }).catch((error) => {
             console.error("Error accessing webcam:", error);
         });
+    }
+
+    let lastVideoTime = -1;
+    let canvasCtx = useRef(null)
+    let drawingUtils = useRef(null)
+
+
+    useEffect(() => {
+        canvasCtx = canvasElement.current.getContext("2d");
+        drawingUtils = new DrawingUtils(canvasCtx);
+    }, [canvasElement]);
+
+    async function predictWebcam() {
+        canvasElement.current.style.height = videoHeight;
+        videoElement.current.style.height = videoHeight;
+        canvasElement.current.style.width = videoWidth;
+        videoElement.current.style.width = videoWidth;
+        // Now let's start detecting the stream.
+        if (runningMode === "IMAGE") {
+            runningMode = "VIDEO";
+            await poseLandmarker.setOptions({runningMode: "VIDEO"});
+        }
+        let startTimeMs = performance.now();
+        if (lastVideoTime !== videoElement.current.currentTime) {
+            lastVideoTime = videoElement.current.currentTime;
+            poseLandmarker.detectForVideo(videoElement.current, startTimeMs, (result) => {
+                canvasCtx.save();
+                canvasCtx.clearRect(0, 0, canvasElement.current.width, canvasElement.current.height);
+                for (const landmark of result.landmarks) {
+                    drawingUtils.drawLandmarks(landmark, {
+                        radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)
+                    });
+                    drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
+                }
+                canvasCtx.restore();
+            });
+        }
+
+        // Call this function again to keep predicting when the browser is ready.
+        if (webcamRunning === true) {
+            window.requestAnimationFrame(predictWebcam);
+        }
     }
 
     return (
@@ -50,11 +111,12 @@ function CounterPage() {
             <h1>Counter!!</h1>
             <div>
                 <video autoPlay playsInline id="webcam" className="h-[720px] w-[1280px] absolute top-10" ref={videoElement}></video>
-                <canvas id="output_canvas" className="absolute top-10" width="1280" height="720"></canvas>
+                <canvas id="output_canvas" className="absolute top-10" width="1280" height="720" ref={canvasElement}></canvas>
             </div>
             <button ref={enableWebcamButton} onClick={enableCam}>Enable webcam</button>
         </>
     )
+
 }
 
-export default CounterPage
+export default CounterPage;
