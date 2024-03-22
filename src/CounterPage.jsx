a37dc9eebe26f21186cf52ce6a739e54837cf94e
roelh <roelhoogendoorn01369@gmail.com>
2024-03-22 13:47:34 +0100

    Refactored code to make it look and work better.

--- src/CounterPage.jsx
+++ src/CounterPage.jsx
@@ -1,23 +1,46 @@
 import {useEffect, useRef, useState} from 'react';
 import {PoseLandmarker, FilesetResolver, DrawingUtils,} from '@mediapipe/tasks-vision'
-import Goal from "./Goal.jsx";
 
 function CounterPage() {
     const videoElement = useRef(null)
     const canvasElement = useRef(null)
-    const [goal, setGoal] = useState(69);
     const enableWebcamButton = useRef(null)
+    let canvasCtx = useRef(null)
+    let drawingUtils = useRef(null)
+    let poseLandmarker = undefined;
     let predictionsRunning = false;
+    let lastVideoTime = -1;
     const videoHeight = "720px";
     const videoWidth = "1280px";
-    let poseLandmarker = undefined;
+    let disableButton = false
 
     let lDown = false
     let rDown = false
     const [lScore, setLScore] = useState(0)
     const [rScore, setRScore] = useState(0)
 
-    // Create tge PoseLandmarker
+    // Set canvasCtx & drawingUtils when the canvasElement has loaded
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        canvasCtx = canvasElement.current.getContext("2d");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        drawingUtils = new DrawingUtils(canvasCtx);
+    }, [canvasElement.current]);
+
+    // Setup user camera
+    function startApp() {
+        const hasGetUserMedia = () => {
+            let _a;
+            return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia);
+        };
+        if (hasGetUserMedia()) {
+            createPoseLandmarker();
+        } else {
+            console.warn("getUserMedia() is not supported by your browser");
+        }
+    }
+
+    // Create PoseLandmarker
     const createPoseLandmarker = async () => {
         const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
         poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
@@ -28,16 +51,11 @@ function CounterPage() {
             runningMode: "VIDEO",
             numPoses: 2
         });
+        enableWebcamButton.current.innerText = "Enable Webcam"
+        enableWebcamButton.current.enabled
     };
-    createPoseLandmarker();
 
-    // Check if webcam access is supported.
-    const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
-    if (!hasGetUserMedia()) {
-        console.warn("getUserMedia() is not supported by your browser");
-    }
-
-    // Enable the live webcam view and start detection.
+    // Enable the live webcam view and start detection
     function enableCam() {
         console.log("start the webcam")
 
@@ -54,55 +72,40 @@ function CounterPage() {
             enableWebcamButton.current.innerText = "DISABLE PREDICTIONS";
         }
 
-        // Activate the webcam stream.
+        // Activate the webcam stream
         navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
             videoElement.current.srcObject = stream;
             videoElement.current.addEventListener("loadeddata", predictWebcam);
+
+            canvasElement.current.style.height = videoHeight;
+            videoElement.current.style.height = videoHeight;
+            canvasElement.current.style.width = videoWidth;
+            videoElement.current.style.width = videoWidth;
         }).catch((error) => {
             console.error("Error accessing webcam:", error);
         });
     }
 
-    let lastVideoTime = -1;
-    let canvasCtx = useRef(null)
-    let drawingUtils = useRef(null)
-    console.log(canvasCtx)
-
-    useEffect(() => {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        canvasCtx = canvasElement.current.getContext("2d");
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        drawingUtils = new DrawingUtils(canvasCtx);
-        console.log(canvasCtx)
-    }, [canvasElement.current]);
-
     async function predictWebcam() {
-        canvasElement.current.style.height = videoHeight;
-        videoElement.current.style.height = videoHeight;
-        canvasElement.current.style.width = videoWidth;
-        videoElement.current.style.width = videoWidth;
-
         let startTimeMs = performance.now();
         if (lastVideoTime !== videoElement.current.currentTime) {
             lastVideoTime = videoElement.current.currentTime;
             poseLandmarker.detectForVideo(videoElement.current, startTimeMs, (result) => {
-                console.log(canvasCtx)
                 canvasCtx.save();
                 canvasCtx.clearRect(0, 0, canvasElement.current.width, canvasElement.current.height);
                 for (const landmark of result.landmarks) {
-                    drawingUtils.drawLandmarks(landmark, {
-                        radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)
-                    });
+                    drawingUtils.drawLandmarks(landmark, {radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)});
                     drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
                 }
                 canvasCtx.restore();
             });
         }
 
-        // Call this function again to keep predicting when the browser is ready.
+        // Call this function again to keep predicting when the browser is ready
         if (predictionsRunning === true) {
             window.requestAnimationFrame(predictWebcam);
 
+            // Code to track and count arm movement
             if (poseLandmarker.landmarks[0] && poseLandmarker.landmarks[0][12].visibility > 0.9 && poseLandmarker.landmarks[0][14].visibility > 0.9 && poseLandmarker.landmarks[0][20].visibility > 0.9) {
                 let lShoulderY = poseLandmarker.landmarks[0][12].y
                 let lElbowY = poseLandmarker.landmarks[0][14].y
@@ -143,16 +146,16 @@ function CounterPage() {
         }
     }
 
+    startApp()
+
     return (
         <div className="min-h-screen bg-gradient-to-br from-[#ff8c00] to-[#ffe312] px-[5vw] pt-[3vh] flex flex-col items-center gap-4">
             <h1 className="text-6xl font-bold">FLEX COUNTER</h1>
-            <Goal goal={goal} setGoal={setGoal}/>
-            <p>{lScore}</p>
-            <p>{rScore}</p>
-            <button className="bg-lime-400 rounded p-2 w-[40vw]" ref={enableWebcamButton} onClick={enableCam}>Enable webcam</button>
+            <p>Left arm: {lScore} | Right arm: {rScore}</p>
+            <button className="bg-lime-400 rounded p-2 w-[40vw]" disabled={disableButton} ref={enableWebcamButton} onClick={enableCam}>Loading...</button>
             <div className="bg-black/25 h-[720px] w-[1280px]">
                 <video autoPlay playsInline id="webcam" className="absolute h-[720px] w-[1280px]" ref={videoElement}></video>
-                <canvas id="output_canvas" className="absolute" width="1280" height="720" ref={canvasElement}></canvas>
+                <canvas id="output_canvas" className="absolute" width={videoWidth} height={videoHeight} ref={canvasElement}></canvas>
             </div>
         </div>
     )
