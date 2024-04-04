9865b532e236a7ac1f56019bb93f5f736d713525
Roel <roelhoogendoorn01369@gmail.com>
2024-04-04 17:28:15 +0200

    Got new datapoints

--- src/getDataPoints.js
+++ src/getDataPoints.js
@@ -10,13 +10,13 @@ export async function getDataPoints(poseLandmarker) {
     data = []
 
     for (let label of ["up", "down", "other"]) {
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 30; i++) {
             await delay(2000)
 
             if (poseLandmarker.landmarks[0]) {
-                let lShoulder = poseLandmarker.landmarks[0][12];
-                let lElbow = poseLandmarker.landmarks[0][14];
-                let lHand = poseLandmarker.landmarks[0][20];
+                let lShoulder = poseLandmarker.landmarks[0][11];
+                let lElbow = poseLandmarker.landmarks[0][13];
+                let lHand = poseLandmarker.landmarks[0][19];
 
                 if (lShoulder.visibility > 0.8 && lElbow.visibility > 0.8 && lHand.visibility > 0.8) {
                     data.push({
